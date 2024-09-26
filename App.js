70d25cbf126fb896cde7a3be8baf8fea3a24198e
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2024-09-27 01:53:11 +0530

    Added some extra things

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex flex-1 dark:bg-gray-950 flex-1">
+      <Main />
+      <Text>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Main.js
+++ Main.js
@@ -1,13 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import "./global.css"
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 const Main = () => {
   return (
-    <Provider>
-      <View>
-        <Text>Main</Text>
-      </View>
-    </Provider>
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text>Main</Text>
+    </View>
   );
-}
-export default Main
-const styles = StyleSheet.create({})
+};
+export default Main;
+const styles = StyleSheet.create({});
